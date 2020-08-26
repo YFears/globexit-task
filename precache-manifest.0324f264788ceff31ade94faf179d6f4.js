71060a0e635bf0c0023210b7cca51df4be615f50
Yury Firsau <janin90@yandex.ru>
2020-08-26 16:43:51 +0300

    Updates

---
+++ precache-manifest.0324f264788ceff31ade94faf179d6f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae476203421f93afc81aa85ec2352c20",
+    "url": "/globexit_task/index.html"
+  },
+  {
+    "revision": "5c5c53e128627d38f000",
+    "url": "/globexit_task/static/css/main.bfa84738.chunk.css"
+  },
+  {
+    "revision": "16f72e12910d24fa3d0b",
+    "url": "/globexit_task/static/js/2.a9c1b0a0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/globexit_task/static/js/2.a9c1b0a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c5c53e128627d38f000",
+    "url": "/globexit_task/static/js/main.971b56e1.chunk.js"
+  },
+  {
+    "revision": "d2b6d7658a3971a8058c",
+    "url": "/globexit_task/static/js/runtime-main.8ddf4528.js"
+  }
+]);
